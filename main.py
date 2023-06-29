f001971b95253e80e7e9522dd1b3072531e1220f
Gruzjan <gracjan122@protonmail.com>
2023-06-29 21:36:38 +0200

    size comparison

--- main.py
+++ main.py
@@ -6,36 +6,39 @@ def getAccount():
     mega = Mega()
     return mega.login(os.getenv('LOGIN'), os.getenv('PASSWORD'))
 
-def createBackupDir(account): #TODO RETHINK
-    paths = os.getenv('PATHS').split(';')
-    folder = account.find('Backups', exclude_deleted=True)
-    if folder is None:
-        for path in paths:
-            account.create_folder(f'Backups/{os.path.basename(path)}')
-    return folder
-            
-# def compareSizes():
-    # createZip()
+def compareSizes(path, account):
+    filename = os.path.basename(path)
+    createZip(path)
+    if account.find(f"{filename}.zip") is None or account.find(f"{filename}.zip")[1]["s"] != os.stat(f"{filename}.zip").st_size:
+        uploadZip(path, account)
+    removeZip(path)
 
-def uploadZip(directory, account):
+def createZip(path):
+    shutil.make_archive(os.path.basename(path), 'zip')
+
+def removeZip(path):
+    os.remove(f"{os.path.basename(path)}.zip")
+
+def uploadZip(path, account):
+    #TODO: Handle old backups
     folder = account.find('Backups', exclude_deleted=True)
-    filename = os.path.basename(directory)
-    shutil.make_archive(filename, 'zip')
+    filename = os.path.basename(path) 
     account.upload(f"{filename}.zip", folder[0])
-    os.remove(f"{filename}.zip")
 
 def main():
     load_dotenv()
     acc = getAccount()
+    paths = os.getenv('PATHS').split(';')
     folder = acc.find('Backups', exclude_deleted=True)
     if folder:
-        #compareSizes()
-        pass
+        for path in paths: 
+            compareSizes(path, acc)
     else:
         acc.create_folder('Backups')
-        paths = os.getenv('PATHS').split(';')
-        for path in paths: 
+        for path in paths:
+            createZip(path)
             uploadZip(path, acc)
+            removeZip(path)
 
 if __name__ == "__main__":
     main()
